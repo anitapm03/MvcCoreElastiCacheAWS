b72739b4687e4ac1dabf559713435ebcd3d47a0a
anitapm03 <apereiramolinero@gmail.com>
2024-05-21 13:21:24 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        public static Lazy<ConnectionMultiplexer>
+            CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                //aqui nuestra cadena de conexion
+                return ConnectionMultiplexer.Connect("");
+            });
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
